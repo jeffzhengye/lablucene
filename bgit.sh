78969f28bde4b597fa616371b3b400ffbfb1be07
jeff <jeff.zheng.ye@gmail.com>
2013-11-12 16:19:56 -0500

    bgit improved

--- bgit.sh
+++ bgit.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+
+
+ant jar; git add src/; git add lib/;
+
+if [ -n "$1" ];
+then
+	git commit -a -m "$1" ;git push
+else
+	git commit -a -m 'minor' ;git push
+fi
+
+exit
 ant jar; git add src/; git add lib/; git commit -a -m 'minor' ;git push
