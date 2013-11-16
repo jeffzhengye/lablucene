db8a5fdfd7db0726632f2c7815179858f49d5221
jeff <jeff.zheng.ye@gmail.com>
2013-11-16 00:55:47 -0500

    minor

--- src/luceneExtend/org/apache/lucene/search/model/MATF.java
+++ src/luceneExtend/org/apache/lucene/search/model/MATF.java
@@ -27,7 +27,7 @@ public class MATF extends WeightingModel {
 	 * @return the name of the model
 	 */
 	public final String getInfo() {
-		return "MATF_MULTIPLE";
+		return "MATF_BRITF";
 	}
 	/**
 	 * Uses BM25 to compute a weight for a term in a document.
@@ -54,7 +54,7 @@ public class MATF extends WeightingModel {
 		float BLRTF = LRTF / (1 + LRTF);
 		
 //		float TFF = alpha * BRITF + (1 - alpha) * BLRTF;
-		float TFF = BRITF * BLRTF;
+		float TFF = BRITF;
 	    
 	    return keyFrequency * TFF * IDF;
 	}
@@ -103,7 +103,7 @@ public class MATF extends WeightingModel {
 			float BLRTF = LRTF / (1 + LRTF);
 			
 //			float TFF = alpha * BRITF + (1 - alpha) * BLRTF;
-			float TFF = BRITF * BLRTF;
+			float TFF = BRITF;
 		    
 		    return keyFrequency * TFF * IDF;
 		}
