79fb8f1cb81b5b3a2a3379b44805f825d0d5047c
jeff <jeff.zheng.ye@gmail.com>
2013-11-22 00:11:29 -0500

    minor

--- src/luceneExtend/org/apache/lucene/search/model/ABM25.java
+++ src/luceneExtend/org/apache/lucene/search/model/ABM25.java
@@ -73,7 +73,7 @@ public class ABM25 extends WeightingModel {
 		float F_t,
 		float keyFrequency) {
 		float alpha = 2 / (1 + Idf.log(1 + this.querylength));
-		b = 0.3f * alpha + 0.7f * (1-alpha);
+		b = 0.7f * alpha + 0.3f * (1-alpha);
 		
 //	    float K = k_1 * ((1 - b) + b * docLength / averageDocumentLength) + tf;
 		
