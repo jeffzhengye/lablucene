c00b25051b5f00db6d5c2995b9b52fe0ec9c1189
jeff <jeff.zheng.ye@gmail.com>
2013-11-30 23:19:05 -0500

    minor

--- src/luceneExtend/org/apache/lucene/search/model/EDLM.java
+++ src/luceneExtend/org/apache/lucene/search/model/EDLM.java
@@ -49,9 +49,8 @@ public class EDLM extends WeightingModel {
 	
 	public final float score(float tf, float docLength, int innerid) {
 		alpha = 2 / (1f + Idf.log(1 + querylength));
-		if(querylength ==1f || querylength==2f){
-			alpha =0;
-		}
+		alpha =0;
+		
 //		float AEF = this.termFrequency/this.documentFrequency;
 //		
 		float RITF = Idf.log(1 + tf)/Idf.log(1 + AvgTF(docLength, innerid));	
